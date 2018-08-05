823c8058a0acc139e9cfed8e5862909ccc2b5a90
Krzysztof Koziarski <krzyhook@gmail.com>
2018-08-05 19:30:56 +0200

    using password from UserSecrets instead of hardcoded environment variable

--- Cert/generate-dev-cert.ps1
+++ Cert/generate-dev-cert.ps1
@@ -1,4 +1,5 @@
-$password = "SecretPassword123" #if you want a random password for each developer machine, use this GUID -> [guid]::newguid()
+$password = [guid]::newguid() #if you want a random password for each developer machine, use this GUID -> [guid]::newguid()
+
 $certPath = "$PSScriptRoot\localhost-dev.pfx"
 #$certPath = "$env:APPDATA\ASP.NET\Https\localhost-dev.pfx"
 Write-Host "Password generated: " $password
@@ -11,4 +12,6 @@ Write-Host "Settings user-secrets for project $projectPath"
 $projectPath = (Convert-Path "$PSScriptRoot/../HttpsAspNetCore/HttpsAspNetCore.csproj")
 
 dotnet user-secrets --project $projectPath set "CertPassword" "$password"
+
+#This setting for CertPath is required for running in VisualStudio, for Docker it is overriden with environment variable
 dotnet user-secrets --project $projectPath set "CertPath" "$certPath"
